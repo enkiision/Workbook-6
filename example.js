d1543858c672d70b2a9dd667066dd2eb63475cd5
Enki Hazzard <ehazzard@appdev.yearup.org>
2024-11-12 16:04:07 -0500

    WB6 assessment notes and codewars starter

---
+++ example.js
@@ -0,0 +1,138 @@
+"use strict"
+
+// let sum = 0;
+// function calculate(a, b, c) {
+//    let sum = a + b + c;
+//    return sum;
+// }
+// calculate(3, 4, 5);
+//---------------------------------
+// let p = 6, q = 2, r = 8;
+// let result = p * q - r / q;
+// console.log(result);
+//----------------------------------------
+// let employee = {
+//     id: "E123",
+//     name: "Jane Smith",
+//     salary: 55000
+//  };
+
+// employee.salary,
+// //employee{salary}
+// employee.salary()
+// employee["55000"]
+// ---------------------------------------------
+// let total = 0;
+// for (let i = 1; i <= 3; i++) {
+//    total += i;
+// }
+// console.log(total);
+//--------------------------------
+// let sum = 0;
+// function add(x,y,z)
+// {
+//     let sum = x + y + z;
+//     return sum;
+// }
+// function display()
+// {
+//     console.log(sum);
+// }
+// ---------------------------------------
+// for(let i = 0; i < 5; i++)
+//     {
+//        // do something
+//        i = i + 1;
+//    }
+
+// for(let i = 0; i < 5; i++)
+// 	{
+// 		// do something
+// 	}
+
+// for(let i = 0; i < 5; i+1)
+// 	{
+// 		// do something
+// 	}
+
+//----------------------------------------------
+//let d = new Date();
+
+// console.log((d.getMonth() + 1) + "/" + d.getDate() + 
+
+//             "/" + d.getFullYear());
+
+//------------------------------------------------------
+
+// let numbers = [0,0,0];
+// function doit(n)
+// {
+//     let num = 10;
+//     for (let i = 1; i < n; i++)
+//     {
+//         num += i;
+//         numbers [i] = num;
+//     }
+// }
+// doit (3);
+
+// console.log(numbers);
+// the answer was none of the above because i got 0,11, 13 which was not an answer
+
+// let a = 5, b = 3, c= 4, d = 2;
+// let answer = a + b * c - d;
+// console.log(answer);  //answer is: 15
+
+// -----------------------------------------------
+
+// let ticketHolder = {
+//     ticketID : "GCIWMY",
+//     tickholder : "chris o' gre",
+//     seats : ["sec e row 22 seat 1",
+//         "sec e row 22 seat 2"]
+// };
+// let amt = 0;
+// for (let o = 0; i < ticketHolder.seats.length; i++)
+// {
+//     amt += 125;
+// }
+// console.log(amt);
+// --------------------------------------------------------------
+// function greet(name){
+//     if(name === "Johnny")
+//         return "Hello, my love!";
+//     return "Hello, " + name + "!";
+//   }
+//--------------------------------------------
+//   function combineNames(first,last){
+//     return first + " " + last;
+//   }
+//   console.log(combineNames("James", "Stevens"));
+  
+//--------------------------------------------------------
+// function checkAlive(health) {
+//     return health > 0;
+//   }
+//----------------------------------------------------------------
+
+// var a = "dev"
+// var b = "Lab"
+
+// var name = a + b
+// console.log(name);
+//-------------------------------------------------------------
+
+// var laLigaGoals = 43
+// var championsLeagueGoals = 10
+// var copaDelReyGoals = 5
+ 
+// var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+// console.log(totalGoals)
+//----------------------------------------------------------
+
+function orderOperations () {
+    return 2 + 2 * 2 + 2 * 2
+  }
+  console.log(orderOperations);
+  
